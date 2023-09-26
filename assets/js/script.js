1b23887e64263629d5767b583b44d8d2c77e85bc
Salvino Modica <salvymody@gmail.com>
2023-09-26 02:57:51 +0000

    feat: add start game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+// Interchangable sections
+const menuSection = document.getElementById("menu-section");
+const gameSection = document.getElementById("game-section")
+
+// Buttons
+const startGameButton = document.getElementById("start-game")
+
+// Event Listeners
+startGameButton.addEventListener("click", startGame);
+
+// Functions
+function startGame(event) {
+    // Closes the menu and opens the game section
+    menuSection.style.display = "none";
+    gameSection.style.display = "flex";
+}
