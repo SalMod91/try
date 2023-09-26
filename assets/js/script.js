c2b0757357271679151e5d8778a625c415327331
Salvino Modica <salvymody@gmail.com>
2023-09-26 03:21:07 +0000

    feat: add choice buttons, images and EL

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,24 @@
 const menuSection = document.getElementById("menu-section");
 const gameSection = document.getElementById("game-section")
 
+// Game Stats
+let playerChoice;
+let computerChoice;
+let drawScore;
+let winScore;
+let loseScore;
+
 // Buttons
 const startGameButton = document.getElementById("start-game")
+const rockButton = document.getElementById("rock-button")
+const paperButton = document.getElementById("paper-button")
+const scissorsButton = document.getElementById("scissors-button")
 
 // Event Listeners
 startGameButton.addEventListener("click", startGame);
+rockButton.addEventListener("click", handleChoice)
+paperButton.addEventListener("click", handleChoice)
+scissorsButton.addEventListener("click", handleChoice)
 
 // Functions
 function startGame(event) {
